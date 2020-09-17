b16d4305849daa5dd6509e3253d0a4b1ed0fbab6
Marripudi Vishnu Sai Vardhan <51715323+vishnu-marripudi10@users.noreply.github.com>
2020-09-17 14:36:12 +0530

    Update MyCalculator.java

--- Day5/src/Com/MyCalculator.java
+++ Day5/src/Com/MyCalculator.java
@@ -4,9 +4,7 @@ package Com;
 import java.util.Scanner;
 
 class MyCalculator {
-	/*
-	 * Create the method long power(int, int) here.
-	 */
+	
 	public long power(int n, int p) throws Exception {
 		if (n == 0 && p == 0)
 			throw new Exception("n and p should not be zero.");
