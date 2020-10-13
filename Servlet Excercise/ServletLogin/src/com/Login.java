f59393774d7d32c4314ea67517bd44c150c0abc3
Marripudi Vishnu Sai Vardhan <51715323+vishnu-marripudi10@users.noreply.github.com>
2020-10-13 12:32:53 +0530

    Add files via upload

---
+++ Servlet Excercise/ServletLogin/src/com/Login.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un=request.getParameter("username");
+		String pw=request.getParameter("password");
+		
+		if(un.equals("admin") && pw.equals("admin"))
+		{
+			response.sendRedirect("success.html");
+			return;
+		}
+		else
+		{
+			response.sendRedirect("error.html");
+			return;
+		}
+	}
+
+}
