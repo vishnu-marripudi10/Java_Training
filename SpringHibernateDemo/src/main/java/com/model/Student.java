e567587e833a4551c8a352691e8cf4f60102cae1
Marripudi Vishnu Sai Vardhan <51715323+vishnu-marripudi10@users.noreply.github.com>
2020-10-13 16:31:25 +0530

    Add files via upload

---
+++ SpringHibernateDemo/src/main/java/com/model/Student.java
@@ -0,0 +1,35 @@
+package com.model;
+
+public class Student {
+
+	private int stdID;
+	private String studentName;
+
+	public Student(int stdID, String studentName) {
+		super();
+		this.stdID = stdID;
+		this.studentName = studentName;
+	}
+
+	public Student() {
+		super();
+
+	}
+
+	public int getStdID() {
+		return stdID;
+	}
+
+	public void setStdID(int stdID) {
+		this.stdID = stdID;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+}
